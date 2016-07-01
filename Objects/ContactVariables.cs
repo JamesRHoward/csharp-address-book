d8d5f07a8ba9a0cffbfbf584498de26f903c1dc9
James Howard <james@>
2016-07-01 08:43:08 -0700

    built contactvariables. homemodules will have changes made

---
+++ Objects/ContactVariables.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+
+namespace AddressBook.Objects
+{
+  public class Contact
+  {
+    private string _name;
+    private string _address;
+    private int _phoneNumber;
+
+    private static List<Contact> _instances = new List <Contact> {};
+
+    public Contact (string name, string address, int phoneNumber)
+    {
+      _name = name;
+      _address = address;
+      _phoneNumber = phoneNumber;
+      _instances.Add(this);
+    }
+    public string GetName()
+    {
+      return _name;
+    }
+    public void SetName(string newName)
+    {
+      _name = newName;
+    }
+    public string GetAddress()
+    {
+      return _address;
+    }
+    public void SetAddress(string newAddress)
+    {
+      _address = newAddress;
+    }
+    public int GetPhoneNumber()
+    {
+      return _phoneNumber;
+    }
+    public void SetPhoneNumber(int newPhoneNumber)
+    {
+      _phoneNumber = newPhoneNumber;
+    }
+    public static List<Contact> GetAll()
+    {
+      return _instances;
+    }
+  }
+}
